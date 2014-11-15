620d0e4f5634eb7a15d85a51c6cf6e09bbf4aa5a
Chris Rapier <rapier@psc.edu>
2014-11-15 15:38:34 -0500

    add support for the NONE cipher
    This patch provides the post authentication switch to the NONE cipher.
    This is useful for the transfer of bulk data via SCP. The NONE switch
    is not allowed during interactive sessions. Both sides of the connection
    *must* support the NONE switch.

--- kex.c
+++ kex.c
@@ -652,6 +652,10 @@ kex_choose_conf(struct ssh *ssh)
 	int nenc, nmac, ncomp;
 	u_int mode, ctos, need, dh_need, authlen;
 	int r, first_kex_follows;
+	int auth_flag;
+
+	auth_flag = packet_authentication_state(ssh);
+	debug ("AUTH STATE IS %d", auth_flag);
 
 	if ((r = kex_buf2prop(kex->my, NULL, &my)) != 0 ||
 	    (r = kex_buf2prop(kex->peer, &first_kex_follows, &peer)) != 0)
@@ -708,6 +712,14 @@ kex_choose_conf(struct ssh *ssh)
 			kex->failed_choice = peer[ncomp];
 			peer[ncomp] = NULL;
 			goto out;
+		}
+		debug("REQUESTED ENC.NAME is '%s'", newkeys->enc.name);
+		if (strcmp(newkeys->enc.name, "none") == 0) {
+			debug("Requesting NONE. Authflag is %d", auth_flag);
+			if (auth_flag == 1)
+				debug("None requested post authentication.");
+			else
+				fatal("Pre-authentication none cipher requests are not allowed.");
 		}
 		debug("kex: %s %s %s %s",
 		    ctos ? "client->server" : "server->client",
